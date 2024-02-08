ae00b57ceb086822f7950995ee02243378783f72
gabrielwere <weregabriel91@gmail.com>
2024-02-08 14:38:42 +0300

    add header files

--- ls/colors.h
+++ ls/colors.h
@@ -1,6 +1,11 @@
+#ifndef COLORS 
+#define COLORS
+
 #define GREEN "\x1b[32m"
 #define BLUE "\x1b[34m"
 #define WHITE "\x1b[37m"
 #define BOLD_ON "\e[1m"
 #define BOLD_OFF "\e[m"
+
+#endif
 
--- ls/helpers.h
+++ ls/helpers.h
@@ -1,14 +1,6 @@
-#include <time.h>
+#ifndef HELPERS
+#define HELPERS
 
-char *give_time(long time_in_seconds){
-  
-	//this is the format of the string returned
-	//Mon Jul 16 02:03:55 1987\n\0
+char *give_time(long time_in_seconds);
 
-	char *time_string = ctime(&time_in_seconds);
-	*(time_string + 16) = '\0';
-	*(time_string + 24) = '\0';
-
-	return time_string;
-
-}
+#endif
