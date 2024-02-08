0804e5d67d02957dcefe7206ed3353be8ceec748
gabrielwere <weregabriel91@gmail.com>
2024-02-08 12:29:49 +0300

    add -R flag handler

--- ls/flags.h
+++ ls/flags.h
@@ -84,6 +84,37 @@ void handle_l_and_R_flag(char *filename){
 
 }
 void handle_R_flag(char *filename){
+	struct stat file_info;
+
+	if(stat(filename,&file_info) == -1){
+		fprintf(stderr,"cannot access \'%s\': No such file or directory",filename);
+		return;
+	}
+
+	if(S_ISDIR(file_info.st_mode)){
+		struct dirent *Dirent;
+		DIR *Dir;
+		
+
+		if((Dir = opendir(filename)) == NULL){
+			fprintf(stderr,"cannot access \'%s\': No such file or directory",filename);
+			return;
+		}
+
+		while((Dirent = readdir(Dir)) != NULL){
+
+			//skip the current directory and the parent directory
+			if((strcmp(".",Dirent->d_name) == 0) || (strcmp("..",Dirent->d_name) == 0))
+				continue;
+
+			char full_path_name[PATH_MAX];
+			sprintf(full_path_name,"%s/%s",filename,Dirent->d_name);
+
+			handle_R_flag(full_path_name);
+		}
+	}else
+		printf("%s\n",filename);
+
 
 }
 
